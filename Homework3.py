f4a0887903cef2726455bff0dc4d5c00a4802fc1
Enes0311 <134113340+Enes0311@users.noreply.github.com>
2023-08-08 06:03:42 +0300

    Add files via upload
    week 3

---
+++ Homework3.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a KNN classifier with k= value
+knn = KNeighborsClassifier(n_neighbors=5)  
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
